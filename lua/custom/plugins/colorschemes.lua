a3a3bf3f67de7f05a27cc2077bad49ba20b7ce00
Jeff Ward <jeff.ward@axuall.com>
2023-11-24 15:58:24 +0000

    Add some more color schemes

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,5 +1,7 @@
 
 return {
+  { 'fenetikm/falcon', name='falcon' },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { 'savq/melange-nvim', name='melange' },
   { 'kepano/flexoki-neovim', name='flexoki' },
 }
